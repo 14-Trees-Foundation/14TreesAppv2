56559d42d6118da3a4483988b3d986497be66abc
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-10-03 21:22:26 +0530

    plot save bug fix.

--- get_tree_plot.js
+++ get_tree_plot.js
@@ -8,7 +8,8 @@ import {
   createUsersTbl,
   updateUsersTbl,
 } from './tree_db';
-
+import {Alert} from 'react-native';
+const failedPlotIds = [];
 export async function fetch_tree_and_plot() {
   try {
     const db = await getDBConnection();
@@ -22,7 +23,17 @@ export async function fetch_tree_and_plot() {
     });
     let plotRes = await axios.get('http://13.235.20.199/api/plots');
     plotRes.data.forEach(async plot => {
-      await updatePlotTbl(db, plot);
+      if(plot.name){
+        plot.name = plot.name.replace("'","''");
+        await updatePlotTbl(db, plot).catch((err)=>{
+          Alert.alert('Plot Save Failure',`Failed to save plot, _id=${plot._id}`);
+          failedPlotIds.push(plot._id);
+        });
+      }
+      else{
+        failedPlotIds.push(plot._id);
+        console.log(plot)
+      }
     });
     let userRes = await axios.get('http://13.235.20.199/api/admin/users/treelogging');
     userRes.data.forEach(async user => {
