b129912755ad6df00e35217b2969c9f826ea7323
Dhruvin-Moradiya <dsmoradiya22@gmail.com>
2025-04-21 11:21:26 +0530

    Build issue

--- src/services/sync/tree.ts
+++ src/services/sync/tree.ts
@@ -126,7 +126,7 @@ export const uploadNewTreesData = async (daoClient: DaoClient, trees: Tree[], sy
                 images.user_card_image && await daoClient.treeImages.markImageUploaded(images.user_card_image.local_id);
                 images.user_tree_image && await daoClient.treeImages.markImageUploaded(images.user_tree_image.local_id);
 
-                if (!response[treeReq.sapling_id].exisitng) syncInfo.trees.add += 1;
+                if (!response[treeReq.sapling_id].existing) syncInfo.trees.add += 1;
                 syncInfo.upload_time = new Date().getTime() - new Date(syncInfo.synced_at).getTime();
                 await saveSyncInfo(daoClient, syncInfo);
             }
